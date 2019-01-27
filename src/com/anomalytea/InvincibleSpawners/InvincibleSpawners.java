dddf99b86e2495005ba97f292b7b7019eb848082
Josh <anomalytea@gmail.com>
2019-01-27 14:07:47 -0800

    Initialize Eclipse project

---
+++ src/com/anomalytea/InvincibleSpawners/InvincibleSpawners.java
@@ -0,0 +1,16 @@
+package com.anomalytea.InvincibleSpawners;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class InvincibleSpawners extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
