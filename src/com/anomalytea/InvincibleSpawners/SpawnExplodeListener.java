911415fe39d67730539df9b80769d7d1266fcafe
AnomalyTea <anomalytea@gmail.com>
2019-02-23 15:21:42 -0800

    Log nearest player to blocked explosion
    Also includes distance to the specified player.

--- src/com/anomalytea/InvincibleSpawners/SpawnExplodeListener.java
+++ src/com/anomalytea/InvincibleSpawners/SpawnExplodeListener.java
@@ -1,9 +1,13 @@
 package com.anomalytea.InvincibleSpawners;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ListIterator;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
@@ -26,8 +30,33 @@ public class SpawnExplodeListener implements Listener {
             + " y=" + String.valueOf(b.getLocation().getBlockY())
             + " z=" + String.valueOf(b.getLocation().getBlockZ())
             + " was protected from being exploded.");
+        System.out.println("Nearest player to explosion: "
+            + getNearestPlayerName(e.getLocation()));
       }
     }
+  }
+
+  private String getNearestPlayerName(Location location) {
+    Player nearestPlayer = null;
+    double nearestDistance = -1;
+
+    for (Player player : plugin.getServer().getOnlinePlayers()) {
+      if (player.getLocation().getWorld().equals(location.getWorld())) {
+        double distance = location.distance(player.getLocation());
+        if (distance < nearestDistance || nearestDistance == -1) {
+          nearestDistance = distance;
+          nearestPlayer = player;
+        }
+      }
+    }
+
+    if (nearestDistance == -1) {
+      return "UNKNOWN";
+    }
+
+    BigDecimal printDistance = new BigDecimal(nearestDistance);
+    printDistance = printDistance.setScale(2, RoundingMode.HALF_UP);
+    return nearestPlayer.getName()+ " (" + printDistance.doubleValue() + " m)";
   }
 
 }
