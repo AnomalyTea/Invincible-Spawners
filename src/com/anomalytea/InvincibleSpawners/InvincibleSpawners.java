b726a3b4886eccc94be29bb3eaa31bb5ca6bc942
AnomalyTea <anomalytea@gmail.com>
2019-02-23 13:51:20 -0800

    Add basic config file framework

--- src/com/anomalytea/InvincibleSpawners/InvincibleSpawners.java
+++ src/com/anomalytea/InvincibleSpawners/InvincibleSpawners.java
@@ -1,10 +1,15 @@
 package com.anomalytea.InvincibleSpawners;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+
 public class InvincibleSpawners extends JavaPlugin {
 
   @Override
   public void onEnable() {
+    saveDefaultConfig();
+    loadConfig();
+
     getServer().getPluginManager().registerEvents(new SpawnBreakListener(), this);
     getServer().getPluginManager().registerEvents(new SpawnExplodeListener(), this);
   }
@@ -12,6 +17,17 @@ public class InvincibleSpawners extends JavaPlugin {
   @Override
   public void onDisable() {
 
+  }
+
+  public ArrayList<String> loadConfig() {
+    ArrayList<String> msg = new ArrayList<>();
+
+    this.reloadConfig();
+
+    msg.add("[" + this.getDescription().getName() + "] Config file loaded.");
+    System.out.println(msg.get(msg.size() - 1));
+
+    return msg;
   }
 
 }
