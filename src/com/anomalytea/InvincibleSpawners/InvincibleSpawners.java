05e7b3876f47bb10f131d81ae455f3b8d3ebda44
AnomalyTea <anomalytea@gmail.com>
2019-02-23 17:25:12 -0800

    Check for updates on mod load

--- src/com/anomalytea/InvincibleSpawners/InvincibleSpawners.java
+++ src/com/anomalytea/InvincibleSpawners/InvincibleSpawners.java
@@ -1,6 +1,11 @@
 package com.anomalytea.InvincibleSpawners;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class InvincibleSpawners extends JavaPlugin {
@@ -15,11 +20,32 @@ public class InvincibleSpawners extends JavaPlugin {
 
     this.getCommand("invinciblespawners").setExecutor(new CommandHandler(this));
     this.getCommand("invinciblespawners").setTabCompleter(new TabComplete());
+
+    if (this.getConfig().getBoolean("check-for-updates")) checkForUpdate();
   }
 
   @Override
   public void onDisable() {
 
+  }
+
+  public void checkForUpdate() {
+    String tag = "[" + this.getDescription().getName() + "] ";
+    try {
+      URL url = new URL("https://drive.google.com/uc?export=download&id=1MXpyzkFFwvqbfdBjV5IQDNUXDF2IlLIQ");
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      conn.setRequestMethod("GET");
+      BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+      String v = rd.readLine();
+      rd.close();
+      if (v.equals(this.getDescription().getVersion())) {
+        System.out.println(tag + "Version is up-to-date.");
+      } else {
+        System.out.println(tag + "Updated version available: " + v);
+      }
+    } catch (IOException e) {
+      System.out.println(tag + "An error occurred while checking for updates.");
+    }
   }
 
   public ArrayList<String> loadConfig() {
@@ -31,6 +57,11 @@ public class InvincibleSpawners extends JavaPlugin {
       this.getConfig().set(
           "creative-can-break",
           this.getConfig().getDefaults().getBoolean("creative-can-break"));
+      this.saveConfig();
+    }
+    if (!this.getConfig().isSet("check-for-updates")) {
+      this.getConfig().set(
+          "check-for-updates", this.getConfig().getDefaults().getBoolean("check-for-updates"));
       this.saveConfig();
     }
 
